02d0b214c4f61b223ef8b3f8a819bda6995669ff
mraysu <mike.ray.sullivan@gmail.com>
2025-05-20 14:03:15 -0700

    basic node

---
+++ team8_pkg/launch/team8_pkg.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package = 'team8_pkg'
+    node_name = 'target_node'
+
+    ld = LaunchDescription()
+
+    calibration_node = Node(
+        package,
+        executable=node_name,
+        output='screen')
+        
+    ld.add_action(calibration_node)
+    return ld
+
